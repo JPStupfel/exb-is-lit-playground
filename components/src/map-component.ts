bd3974f766fba8c1d05f64c5781562485eddbd08
JP Stupfel <jstupfel@esri.com>
2024-12-04 15:33:52 -0700

    feat(mapComponent): click for lat lng

--- components/src/map-component.ts
+++ components/src/map-component.ts
@@ -8,20 +8,56 @@ import { customElement, property } from 'lit/decorators.js';
 export class MapComponent extends LitElement {
   static override styles = css`
     :host {
+      #componentWrapper {
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        margin-top: 10px;
+      }
     }
   `;
 
   @property({ type: Object }) map = {};
   @property({ type: String }) item = '';
+  @property({ type: Number }) lat = 0;
+  @property({ type: Number }) lng = 0;
+
+  private _handleClick() {
+    // @ts-ignore
+  }
+
+  private _handleMapClick() {
+    // @ts-ignore
+    this.map.on('click', (e) => {
+      this.lat = e.mapPoint.latitude;
+      this.lng = e.mapPoint.longitude;
+    });
+    // @ts-ignore
+  }
 
   override render() {
+    this._handleMapClick();
     return html`
-      <button @click=${() => console.log('map', this.map)}>Log Map</button>
+      <div id="componentWrapper">
+        <button @click=${this._handleClick}>Log Map</button>
+        <div>
+          Lat:
+          ${
+            // @ts-ignore
+            this.lat
+          }
+        </div>
+        <div>
+          Lng:
+          ${
+            // @ts-ignore
+            this.lng
+          }
+        </div>
+      </div>
     `;
   }
 }
-// <button @click=${() => console.log('item', JimuMapView)}>Log Item</button>
-// <div>Zoom Level: ${this.map?.view?.zoom ?? 'N/A'}</div>
 
 declare global {
   interface HTMLElementTagNameMap {
